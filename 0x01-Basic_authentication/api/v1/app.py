0a3ce483c7e53201f8c463d4fe42880f58764bef
jxkimathi <judescriva8@gmail.com>
2024-08-06 18:37:47 +0300

    BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,35 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+if os.getenv('AUTH_TYPE') == 'auth':
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
+
+
+@app.before_request
+def before_request():
+    """ Before request handler
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 @app.errorhandler(401)
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""
-Basic Auth class thta inherits from Auth
-"""
+"""Basic Auth class thta inherits from Auth"""
 
 from api.v1.auth.auth import Auth
 from typing import TypeVar
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class
-    """
+    """ BasicAuth class"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """ extract_base64_authorization_header
-        """
+        """ extract_base64_authorization_header"""
         if authorization_header is None or type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':
@@ -21,8 +17,7 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        """ decode_base64_authorization_header
-        """
+        """ decode_base64_authorization_header"""
         if base64_authorization_header is None or type(base64_authorization_header) is not str:
             return None
         try:
@@ -35,8 +30,7 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
-        """ extract_user_credentials
-        """
+        """ extract_user_credentials"""
         if decoded_base64_authorization_header is None or \
                 type(decoded_base64_authorization_header) is not str or \
                 ':' not in decoded_base64_authorization_header:
@@ -44,8 +38,7 @@ class BasicAuth(Auth):
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """ user_object_from_credentials
-        """
+        """ user_object_from_credentials"""
         if user_email is None or type(user_email) is not str or \
                 user_pwd is None or type(user_pwd) is not str:
             return None
@@ -59,8 +52,7 @@ class BasicAuth(Auth):
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
-        """
+        """ current_user"""
         auth_header = self.authorization_header(request)
         base64_header = self.extract_base64_authorization_header(auth_header)
         decoded_header = self.decode_base64_authorization_header(base64_header)
