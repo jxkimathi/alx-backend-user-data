903c86d288e440840f7aea0090fb06159adefb5a
jxkimathi <judescriva8@gmail.com>
2024-08-15 13:12:19 +0300

    implementing the regster_user method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def json_payload():
     """Basic route"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Registers a new user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": f"{email}", "message": "user created"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
