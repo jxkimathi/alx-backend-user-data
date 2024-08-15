c7f34d8c5c35fc15be6db92d04c6b1f021701d6c
jxkimathi <judescriva8@gmail.com>
2024-08-15 18:16:25 +0300

    changing the valid login code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,18 +33,12 @@ class Auth:
         raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
-        """ Credentials validation """
-        user = None
+        """Validates a user's login credentials"""
         try:
             user = self._db.find_user_by(email=email)
-            if user is not None:
-                return bcrypt.checkpw(
-                    password.encode("utf-8"),
-                    user.hashed_password,
-                )
+            return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
         except NoResultFound:
             return False
-        return False
 
     def create_session(self, email: str) -> str:
         """Retrieves a user based on a given session ID."""
