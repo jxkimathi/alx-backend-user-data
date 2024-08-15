49a6a496384ceb31217ac4a1692bd6ae124682ad
jxkimathi <judescriva8@gmail.com>
2024-08-15 15:23:36 +0300

    implementing the valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,9 +23,20 @@ class Auth:
 
     def register_user(self, email: str, password:str) -> User:
         """Registers a new user"""
+        user = self._db.find_user_by(email=email)
+        hashed_password = _hash_password(password)
+        if user:
+            raise ValueError(f'User {email} already exists')
+        return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Credentials validation """
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            return self._db.add_user(email, hashed_password)
+            user = self._db.find_user_by(email=email)
+            if user:
+                user_pw = user.hashed_password
+                input_password = password.encode("utf-8")
+                return bcrypt.checkpw(input_password, user_pw)
+        except (NoResultFound, InvalidRequestError):
+            return False
+        return False
