4b79bf6b90d2579bcbbd16ddad7b5c64244dd9bd
jxkimathi <judescriva8@gmail.com>
2024-08-15 13:30:42 +0300

    editing the return to be in the try block

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,21 +8,21 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def index():
+def index() -> str:
     """Basic route"""
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'])
-def users():
+def users() -> str:
     """Registers a new user"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
